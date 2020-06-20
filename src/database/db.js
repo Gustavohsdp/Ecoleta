7afd4de38a18337c1a9d6d032e24bd30fa2c093d
Gustavohsdp <gustavohsdp@gmail.com>
2020-06-20 12:02:27 -0300

    Backend in progress

--- src/database/db.js
+++ src/database/db.js
@@ -4,56 +4,72 @@ const sqlite3 = require("sqlite3").verbose()//Verbose mostrar informações no t
 //criar o objeto que ira operações no banco de dados
 const db = new sqlite3.Database("./src/database/database.db")
 
+//Exporta o banco de dados
+module.exports = db
 //utilizar o objeto de banco de dados para nossas operações
-db.serialize(() => {
+//db.serialize(() => {
     //Com comandos SQL
 
     // 1º Criar uma tabela com comandos SQL
-    db.run(`
-        CREATE TABLE IF NOT EXISTS places (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            image TEXT,
-            name TEXT,
-            address TEXT,
-            address2 TEXT,
-            state TEXT,
-            city TEXT,
-            items TEXT
-        );
-    `)
+    //db.run(`
+        //CREATE TABLE IF NOT EXISTS places (
+            //id INTEGER PRIMARY KEY AUTOINCREMENT,
+            //image TEXT,
+            //name TEXT,
+            //address TEXT,
+            //address2 TEXT,
+            //state TEXT,
+            //city TEXT,
+            //items TEXT
+        //)
+    //`)
     //2º inserir dados na tabela
-    const query = `
-        INSERT INTO places (
-            image,
-            name,
-            address,
-            address2,
-            state,
-            city,
-            items
-        ) VALUES (?,?,?,?,?,?,?);
-    `
-    const values = [
-        "https://images.unsplash.com/photo-1528323273322-d81458248d40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1101&q=80",
-        "Colectoria",
-        "Guilherme Gemballa, Jardim América",
-        "Nº 260",
-        "Santa Catarina",
-        "Rio do Sul",
-        "Resíduos Eletrônicos, Lâmpadas"
-    ]
+    //const query = `
+        //INSERT INTO places (
+            //image,
+            //name,
+            //address,
+            //address2,
+            //state,
+            //city,
+            //items
+        //) //VALUES (?,?,?,?,?,?,?);
+    //`
+    //const values = [
+        //"https://images.unsplash.com/photo-1526951521990-620dc14c214b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80",
+        //"Papersider",
+        //"Guilherme Gemballa, Jardim América",
+        //"Nº 260",
+        //"Santa Catarina",
+        //"Rio do Sul",
+        //"Papéis e Papelão"
+    //]
 
-    function afterInsertData(err) {
-        if(err) {
-            return console.log(err)
-        }
-        console.log("Cadastrado com sucesso")
-        console.log(this)
-    }
+    //function afterInsertData(err) {
+        //if (err) {
+            //return console.log(err)
+        //}
+        //console.log("Cadastrado com sucesso")
+        //console.log(this)
+    //}
 
-    db.run(query,values, afterInsertData)
+    //db.run(query, values, afterInsertData)
 
     //3º consultar dados na tabela
-
+    //   db.all(`SELECT name FROM places`, function(err, rows) {
+    //        if(err) {
+    //            return console.log(err)
+    //        }
+    //        console.log("Aqui estão os registros: ")
+    //        console.log(rows)
+    //    })
     //4º deletar um dado na tabela
-})
+
+    //    db.run(`DELETE FROM places WHERE id = ?`, [1], function(err) {
+    //        if(err) {
+    //            return console.log(err)
+    //       }
+
+    //        console.log("Registro deletado com sucesso !")
+    //   })
+//})
--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const exprees = require("express") //Chamando o express
 const server = exprees() //Executando a função exprees no servidor
 
+//pegar o banco de dados
+const db = require("./database/db.js")
+
 
 //Configurar pasta publica
 server.use(exprees.static("public"))
@@ -21,13 +24,28 @@ nunjucks.configure("src/views", {
 server.get("/", (req, res) => {
     return res.render("index.html", { title: "Seu marktplace de coleta de resíduos" })
 })
+
 server.get("/create-point", (req, res) => {
     return res.render("create-point.html")
 })
-server.get("/search", (req, res) => {
-    return res.render("search-results.html")
-})
 
+server.get("/search", (req, res) => {
+
+    // pegar os dados do banco de dados
+    db.all(`SELECT * FROM places`, function(err, rows) {
+                if(err) {
+                    return console.log(err)
+                }
+                console.log("Aqui estão os registros: ")
+                console.log(rows)
+
+                // contar quantos elementos tenho dentro do array
+                const total = rows.length
+
+                //mostrar a página HTML com os dados do banco de dados
+                return res.render("search-results.html", { places: rows, total: total})
+            })
+})
 
 //npm install nodem: para reiniciar o servidor automaticamente
 //ligar o servidor
