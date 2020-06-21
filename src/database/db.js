39cb486f4909ba15b86692afe5914e09fde7efef
Gustavohsdp <gustavohsdp@gmail.com>
2020-06-21 00:19:39 -0300

    Completed project

--- src/database/db.js
+++ src/database/db.js
@@ -4,72 +4,75 @@ const sqlite3 = require("sqlite3").verbose()//Verbose mostrar informações no t
 //criar o objeto que ira operações no banco de dados
 const db = new sqlite3.Database("./src/database/database.db")
 
-//Exporta o banco de dados
 module.exports = db
 //utilizar o objeto de banco de dados para nossas operações
-//db.serialize(() => {
+db.serialize(() => {
     //Com comandos SQL
 
     // 1º Criar uma tabela com comandos SQL
-    //db.run(`
-        //CREATE TABLE IF NOT EXISTS places (
-            //id INTEGER PRIMARY KEY AUTOINCREMENT,
-            //image TEXT,
-            //name TEXT,
-            //address TEXT,
-            //address2 TEXT,
-            //state TEXT,
-            //city TEXT,
-            //items TEXT
-        //)
-    //`)
-    //2º inserir dados na tabela
-    //const query = `
-        //INSERT INTO places (
-            //image,
-            //name,
-            //address,
-            //address2,
-            //state,
-            //city,
-            //items
-        //) //VALUES (?,?,?,?,?,?,?);
-    //`
-    //const values = [
-        //"https://images.unsplash.com/photo-1526951521990-620dc14c214b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80",
-        //"Papersider",
-        //"Guilherme Gemballa, Jardim América",
-        //"Nº 260",
-        //"Santa Catarina",
-        //"Rio do Sul",
-        //"Papéis e Papelão"
-    //]
+db.run(`
+        CREATE TABLE IF NOT EXISTS places (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            image TEXT,
+            name TEXT,
+            address TEXT,
+            address2 TEXT,
+            state TEXT,
+            city TEXT,
+            items TEXT
+        )
+    `)
+        // // 2 Inserir dados na tabela
+    // const query = `
+    //     INSERT INTO places (
+    //         image,
+    //         name,
+    //         address,
+    //         address2,
+    //         state,
+    //         city,
+    //         items
+    //     ) VALUES (?,?,?,?,?,?,?);
+    // `
 
-    //function afterInsertData(err) {
-        //if (err) {
-            //return console.log(err)
-        //}
-        //console.log("Cadastrado com sucesso")
-        //console.log(this)
-    //}
+    // const values = [
+    //     "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+    //     "Papersider",
+    //     "Guilherme Gemballa, Jardim América",
+    //     "Número 260",
+    //     "Santa Catarina",
+    //     "Rio do Sul",
+    //     "Resíduos Eletrônicos, Lâmpadas"
+    // ]
 
-    //db.run(query, values, afterInsertData)
+    // function afterInsertData(err) {
+    //     if(err) {
+    //         return console.log(err)
+    //     }
 
-    //3º consultar dados na tabela
-    //   db.all(`SELECT name FROM places`, function(err, rows) {
-    //        if(err) {
-    //            return console.log(err)
-    //        }
-    //        console.log("Aqui estão os registros: ")
-    //        console.log(rows)
-    //    })
-    //4º deletar um dado na tabela
+    //     console.log("Cadastrado com sucesso")
+    //     console.log(this)
+    // }
 
-    //    db.run(`DELETE FROM places WHERE id = ?`, [1], function(err) {
-    //        if(err) {
-    //            return console.log(err)
-    //       }
+    // db.run(query, values, afterInsertData)
 
-    //        console.log("Registro deletado com sucesso !")
-    //   })
-//})
+
+    // 3 Consultar os dados da tabela
+    // db.all(`SELECT name FROM places`, function(err, rows) {
+    //     if(err) {
+    //         return console.log(err)
+    //     }
+
+    //     console.log("Aqui estão seus registros: ")
+    //     console.log(rows)
+    // })
+
+    // 4 Deletar um dado da tabela
+    // db.run(`DELETE FROM places WHERE id = ?`, [3], function(err) {
+    //     if(err) {
+    //         return console.log(err)
+    //     }
+
+    //     console.log("Registro deletado com sucesso!")
+    // })
+})
--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ const db = require("./database/db.js")
 //Configurar pasta publica
 server.use(exprees.static("public"))
 
+// habilitar o uso do re.body na nosssa aplicação
+server.use(exprees.urlencoded({ extended: true }))
 
 //utilizando template engine nunjucks
 const nunjucks = require("nunjucks")
@@ -26,25 +28,76 @@ server.get("/", (req, res) => {
 })
 
 server.get("/create-point", (req, res) => {
+    //req.query:Query Strings da nossa url
+    //console.log(req.query)
     return res.render("create-point.html")
 })
 
+server.post("/savepoint", (req, res) => {
+    //re.body: O corpo do nosso formulario
+    //console.log(req.body)
+
+    //inserir dados no banco de dados
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+    `
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData(err) {
+        if(err) {
+            console.log(err)
+            return res.send("Erro no cadastro!")
+        }
+
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+
+        return res.render("create-point.html", {saved: true})
+    }
+
+    db.run(query, values, afterInsertData)
+})
+
+
 server.get("/search", (req, res) => {
 
+    const search = req.query.search
+
+    if(search == "") {
+        //pesquisa vazia
+        return res.render("search-results.html", {total: 0 })
+    }
+
     // pegar os dados do banco de dados
-    db.all(`SELECT * FROM places`, function(err, rows) {
-                if(err) {
-                    return console.log(err)
-                }
-                console.log("Aqui estão os registros: ")
-                console.log(rows)
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function (err, rows) {
+        if (err) {
+            return console.log(err)
+        }
+        console.log("Aqui estão os registros: ")
+        console.log(rows)
 
-                // contar quantos elementos tenho dentro do array
-                const total = rows.length
+        // contar quantos elementos tenho dentro do array
+        const total = rows.length
 
-                //mostrar a página HTML com os dados do banco de dados
-                return res.render("search-results.html", { places: rows, total: total})
-            })
+        //mostrar a página HTML com os dados do banco de dados
+        return res.render("search-results.html", { places: rows, total: total })
+    })
 })
 
 //npm install nodem: para reiniciar o servidor automaticamente
